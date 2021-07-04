31564f17ab7f2bd10ec85c6ef2c492c883a59d35
leejanghe <babybye777@gmail.com>
2021-07-05 02:02:09 +0900

    done

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styles from './video_item.module.css';
 
-const videoItem = (props) => (
-            <h1>{props.video.snippet.title}</h1>
+const VideoItem = ({video:{snippet}}) => ( //props.video.snippet 반복되니까 걍 구조분해할당함
+            <div className={styles.container}>
+            <li className={styles.video}>
+                <img className={styles.thumbnails} src={snippet.thumbnails.medium.url} 
+                alt="video thumbnail"
+                />
+                <div className={styles.metadata}>
+                    <p className={styles.title}>{snippet.title}</p>
+                    <p className={styles.channel}>{snippet.channelTitle}</p>
+                </div>
+            </li>
+            </div>
+
     );
 
-export default videoItem;
+export default VideoItem;
