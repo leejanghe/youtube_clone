977100cbbef2ac5c65584368f8f72633143c6aa3
leejanghe <babybye777@gmail.com>
2021-07-06 02:00:11 +0900

    youtube clone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, {useEffect, useState} from 'react';
-import './App.css';
+import styles from'./app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 function App() {
   const [videos, setVideose] = useState([])
-  
+  const search = query =>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyC9zKudUmIt8ffXIVgu6BjcTUS5mfO8YeM&key=AIzaSyC9zKudUmIt8ffXIVgu6BjcTUS5mfO8YeM`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({...item ,id:item.id.videoId})))
+      .then(items => setVideose(items))
+      .catch(error => console.log('error', error));
+  }
 
   useEffect(()=>{
     console.log('effect입니다.')
@@ -20,7 +32,10 @@ function App() {
   },[])
 
   return (
-   <VideoList videos={videos} />
+    <div className={styles.app}>
+    <SearchHeader onSearch={search} />
+    <VideoList videos={videos} />
+   </div>
   );
 }
 
