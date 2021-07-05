24f237f44060842dad0b7c1cb1fc4ef6999c4c9e
leejanghe <babybye777@gmail.com>
2021-07-06 03:34:24 +0900

    youtube done

--- src/App.js
+++ src/App.js
@@ -3,32 +3,37 @@ import styles from'./app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
-  const [videos, setVideose] = useState([])
+function App({youtube}) {
+  const [videos, setVideos] = useState([])
   const search = query =>{
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
+    youtube.search(query)//
+    .then(videos => setVideos(videos))
+
+    // const requestOptions = {
+    //   method: 'GET',
+    //   redirect: 'follow'
+    //};
     
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyC9zKudUmIt8ffXIVgu6BjcTUS5mfO8YeM&key=AIzaSyC9zKudUmIt8ffXIVgu6BjcTUS5mfO8YeM`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({...item ,id:item.id.videoId})))
-      .then(items => setVideose(items))
-      .catch(error => console.log('error', error));
+    // fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyC9zKudUmIt8ffXIVgu6BjcTUS5mfO8YeM&key=AIzaSyC9zKudUmIt8ffXIVgu6BjcTUS5mfO8YeM`, requestOptions)
+    //   .then(response => response.json())
+    //   .then(result => result.items.map(item => ({...item ,id:item.id.videoId})))
+    //   .then(items => setVideose(items))
+    //   .catch(error => console.log('error', error));
   }
 
   useEffect(()=>{
-    console.log('effect입니다.')
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
+    youtube.mostPopular()//
+    .then(videos => setVideos(videos))
+    // console.log('effect입니다.')
+    // const requestOptions = {
+    //   method: 'GET',
+    //   redirect: 'follow'
+    // };
     
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyC9zKudUmIt8ffXIVgu6BjcTUS5mfO8YeM&key=AIzaSyC9zKudUmIt8ffXIVgu6BjcTUS5mfO8YeM", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideose(result.items))
-      .catch(error => console.log('error', error));
+    // fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyC9zKudUmIt8ffXIVgu6BjcTUS5mfO8YeM&key=AIzaSyC9zKudUmIt8ffXIVgu6BjcTUS5mfO8YeM", requestOptions)
+    //   .then(response => response.json())
+    //   .then(result => setVideose(result.items))
+    //   .catch(error => console.log('error', error));
   },[])
 
   return (
